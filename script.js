e3e6a19d0d61d61db3184249cfe05c9b71d461ee
Aleksandre Lashauri <aleksandrelashauri@gmail.com>
2021-04-30 18:54:00 +0400

    ex3

--- script.js
+++ script.js
@@ -1,104 +1,133 @@
-let subject = ['Math' , 'Biology', 'Geography', 'History'];
+class Table{
+    header = [];
+    tableId = '';
+    constructor(id, header){
+        this.tableId = id;
+       this.header = header;
+       this.generateHead();
+    }
+    //for add Row
+    addRow(student, classes) {
+        let tbody = document.querySelector(`#${this.tableId} tbody`);
+        let tmp = `<tr>
+        <td>${student.name}</td>
+        <td>${student.lastname}</td>
+        `;
+        for (let j = 0; j < student.scores.length; j++) {
+            tmp += `<td class="${classes}">${student.scores[j]}</td>`;
+        }
+        tmp += '</tr>';
+        tbody.innerHTML += tmp;
+        return true;
+    }
+    //for add haeder
+    generateHead() {
+        let thead = document.querySelector(`#${this.tableId} thead tr`);
+        let tmp = '';
+        for (let i = 0; i < this.header.length; i++) {
+            tmp += `<th>${this.header[i]}</th>`;
+        }
+        thead.innerHTML = tmp;
+    }
+    //  (??)
+   generateFooter(){
+   }
+    
+}
+class StudentsList extends Table{
+    subjects = [];
+    students = [];
+    avg = [];
+    constructor(students, subjects){
+        let header = ['Name', 'Lastname'];
+        super('container', [...header, ...subjects]);
+        this.students = students;
+        this.subjects = subjects;
+        this.eventListeners();
+        this.generateAvg();
+        this.generateTable();
+    }
+    eventListeners(){
+        document.getElementById('add_student_form').addEventListener('submit', this.addStudent.bind(this));
+    }
+    generateTable(){
 
+        this.generateHead();
+        this.generateFooter();
+        for (let i = 0; i < students.length; i++) {
+            this.addRow(students[i]);
+        }
+    }
+//footer
+    generateFooter(){
+        let tmp = '<tfoot>' +
+            '<tr>' +
+            '<td colspan="2">Avarage</td>';
+        for (let i = 0; i < this.avg.length; i++) {
+            tmp += `<td>${this.avg[i]}</td>`;
+        }
+        tmp += '</tr>' +
+            '</tfoot>';
+        document.querySelector('#container tfoot').innerHTML = tmp;
+    }
+  //avarage
+     generateAvg() {
+     
+        for (let i = 0; i < this.students.length; i++) { 
+            for (let j = 0; j < this.students[i].scores.length; j++) {
+                this.avg[j] = this.avg[j] ? this.avg[j] + this.students[i].scores[j] : this.students[i].scores[j];
+            }
+        }
+        for (let i = 0; i < this.avg.length; i++) {
+            this.avg[i] /= this.students.length;
+        }
+    }
+    addStudent(e) {
+        e.preventDefault();
+        let fields = document.querySelectorAll('#add_student_form input');
+        let newStudent = {
+            name: '',
+            lastname: '',
+            scores: []
+        };
+        for (let i = 0; i < fields.length; i++) {
+            if (fields[i].name === 'name') {
+                newStudent.name = fields[i].value;
+            } else if (fields[i].name === 'lastname') {
+                newStudent.lastname = fields[i].value;
+            } else {
+                newStudent.scores.push(parseInt(fields[i]. value) ? parseInt(fields[i].value) : 0);
+            }
+        }
+        this.students.push(newStudent);
+        this.addRow(newStudent);
+        this.generateAvg();
+        return true;
+    }
+}
+let subjects = ['html', 'js', 'math', 'css'];
 let students = [
-    { 
-        name :'Gio' , 
-        lastname:'aaa' ,
-        scores :[21 , 51, 70, 10]
-    },
-    { 
-        name :'Nika' ,  
-        lastname:'bbb', 
-        scores :[51 ,41, 40, 50 ]
-    },
-    { 
-        name :'Sopo' ,  
-        lastname:'ccc',
-        scores :[53 , 51,  30, 60 ]
+    {
+        name: 'one',
+        lastname: 'aaa',
+        scores: [85, 30, 9, 82]
     },
     {
-         name :'Salome',  
-         lastname:'ddd',
-         scores :[61 , 31, 50, 70 ]
+        name: 'two',
+        lastname: 'bbb',
+        scores: [15, 31, 29, 27]
+    },
+    {
+        name: 'three',
+        lastname: 'ggg',
+        scores: [65, 34, 69, 57]
+    },
+    {
+        name: 'four',
+        lastname: 'ddd',
+        scores: [35, 34, 69, 52]
     }
-   
 ];
-let avg=[0,0,0,0];
-function generateAvg(){
-//calculate avarage
-let avg=[0,0,0,0];
+new StudentsList(students, subjects);
 
-for (let i=0; i<students.length; i++){
-    for(let j=0; j<subject.length; j++){
-        avg[j]+= students[i].scores[j];
-    }
-}
-for (let i=0; i<avg.length; i++){
-    avg[i] /= students.length;
-}
-//generate html avarages
-let  tmp='<tfoot>'+
-    '<tr>'+
-    '<td colspan="2"> Avarage </td>';
 
-for(let i=0; i < avg.length; i++){
-    tmp+=`<td>${avg[i]}</td>`;
-}
-tmp += '</tr>'+
-    '</tfoot>';
-document.querySelector('#container tfoot').innerHTML=tmp;
-
-}
-function generateHead(){
-let thead= document.querySelector('#container thead tr');
-let tmp= '<th>Name</th><th>Lastname</th>';
-for (let i=0; i<subject.length; i++){
-    tmp+=`<th>${subject[i]}</th>`;
-}
-thead.innerHTML = tmp;
-}
-function addRow(student){
-let tbody = document.querySelector('#container tbody');
-let tmp = `<tr>
-            <td>${student.name}</td>
-            <td>${student.lastname}</td>
-            `;
-for (let j = 0; j <student.scores.length; j++){
- tmp += `<td class="${avg[j] > student.scores[j] ? 'red' : 'green' }"> ${student.scores[j]}</td>`;
-}
-tmp += '</tr>';
-
-tbody.innerHTML+=tmp;
-return true;
-}
-function addStudent(e){
-e.preventDefault();
-let fields= document.querySelectorAll('#add_student_form input');
-let newStudent= { 
-    name:'',
-    lastname:'',
-    scores: []
-};
-for(let i=0; i< fields.length; i++){
-    if (fields[i].name==='name'){
-        newStudent.name=fields[i].value;
-    }else if (fields[i].name === 'lastname'){
-        newStudent.lastname=fields[i].value;
-    }else{
-        newStudent.scores.push(parseInt(fields[i].value) ? parseInt(fields[i].value):0);
-    }
-}
-students.push(newStudent);
-addRow(newStudent);
-generateAvg();
-return true;
-}
-function init(){
-document.getElementById('add_student_form').addEventListener('submit', addStudent);
-generateHead(); 
-generateAvg();
-for(let i=0; i< students.length; i++){
-    addRow(students[i]);
-    }
-}
-init();
