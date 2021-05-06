2b5123eb762d75e2b00d789cecb97568d7d0de19
Aleksandre Lashauri <aleksandrelashauri@gmail.com>
2021-05-06 15:33:08 +0400

    fetch done

--- script.js
+++ script.js
@@ -1,38 +1,41 @@
-const myForm=document.getElementById('myForm');
-myForm.addEventListener('submit',function(e){
-    e.preventdefault();
-});
+const search = document.querySelector(".search-button");
+const musicList=document.querySelector(".form");
+const myTube=document.querySelector(".myTube");
+search.addEventListener('click', fetchData);
 
 function fetchData(){
-fetch("https://itunes.apple.com/search?term=gunsnroses.")
+let searchInput= document.querySelector('.input-class').value;
+fetch(`https://itunes.apple.com/search?term=${searchInput}`)
 .then(response => {
     if(!response.ok){
         throw Error("ERROR");
     }  
     return response.json();
 })
+//generation videos
 .then(results => {
     const html = results.results
     .map(results => {
         return `
-        <div class= "results">
-        <p> Name: ${results.artistName}</p>
-        <p> track Name: ${results.trackName}</p>
-        <p> country: ${results.country}</p>
-        <p> artist View Url: ${results.artistViewUrl}</p>
+        <div class="card">
+        <div class="text-container">
+             <video class ="video" controls>
+             <source src="${results.previewUrl}" type="video/mp4"> 
+             </video>
+        <div class="details"> ${results.artistName}</div>
+        <div class="details"> ${results.trackName}</div>
+        <div class="details"> ${results.country}</div>
         </div>
         `;
     })
     .join("");
     console.log(html);
     document.querySelector("#info").insertAdjacentHTML("afterbegin",html);
-
 })
 .catch(error =>{
     console.log(error);
 });
 }
-
 fetchData();
 
 
